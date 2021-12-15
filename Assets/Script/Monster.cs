0af0d414292d3e02fb6d3558da01f6cd61bc020d
Insanitori <92389074+Insanitori@users.noreply.github.com>
2021-12-15 14:24:30 -0500

    gbj

--- Assets/Script/Monster.cs
+++ Assets/Script/Monster.cs
@@ -7,7 +7,6 @@ public class Monster : MonoBehaviour
 {
     public GameObject player;
     public Movement Move;
-    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
